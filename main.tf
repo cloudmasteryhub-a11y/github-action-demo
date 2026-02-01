82e420f0b5437120e43e5aae72ee535c45ce8175
Maheswaran <maheswaran@Maheswarans-MacBook-Air.local>
2026-02-01 10:03:13 -0600

    ec2

---
+++ main.tf
@@ -0,0 +1,32 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+  backend "s3" {
+    bucket = "my-terraform-state"
+    key    = "ec2/terraform.tfstate"
+    region = "us-east-1"
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "web_server" {
+  ami           = "ami-0c55b159cbfafe1f0"  # Amazon Linux 2
+  instance_type = "t2.micro"
+  
+  tags = {
+    Name        = "web-server"
+    Environment = "production"
+    ManagedBy   = "Terraform"
+  }
+}
+
+output "instance_public_ip" {
+  value = aws_instance.web_server.public_ip
+}

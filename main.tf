3d642f492b4dabe2d6f7e5bce96c6ad4ee71ec20
Maheswaran <maheswaran@Maheswarans-MacBook-Air.local>
2026-02-07 00:17:28 -0600

    updae ami

--- main.tf
+++ main.tf
@@ -17,8 +17,18 @@ provider "aws" {
   region = "us-east-1"
 }
 
+data "aws_ami" "amazon_linux_2" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+  }
+}
+
 resource "aws_instance" "web_server" {
-  ami           = "ami-0c55b159cbfafe1f0"  # Amazon Linux 2
+  ami           = data.aws_ami.amazon_linux_2.id
   instance_type = "t2.micro"
   
   tags = {
